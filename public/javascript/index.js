2f6582b350e6fbf48de8200c7bdc52ff5a526341
Suzal3579 <pe.messh@gmail.com>
2017-11-14 22:55:18 +0530

    commit for testing in heroku ...

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on("connect", () => {
     console.log("Connected to server");
-    socket.emit("createMessage", {
-        from: "suzal",
-        text: "This is from client",
-        date: new Date()
-    });
 });
 
 socket.on("disconnect", () => {
--- server/server.js
+++ server/server.js
@@ -16,16 +16,23 @@ server.listen(port, () => {
 
 io.on("connection", (socket) => {
     console.log(`Socket ${socket.id} connected`);
-
     socket.emit("newMessage", {
-        from: "pe.messh@gmail.com",
-        text: "This is a demo text from Pramesh",
-        createdAt: new Date()
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "New User Joined",
+        createdAt: new Date().getTime()
     });
 
     socket.on("createMessage", (message) => {
-        console.log("Create message");
-        console.log(message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on("disconnect", () => {
