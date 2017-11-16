b8500a7f5b86a53ac9704b0dd24ba065d8c4e5fe
Suzal3579 <pe.messh@gmail.com>
2017-11-16 13:01:04 +0530

    scrollings is tricky :D

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,25 @@
 const socket = io();
+
+const scrollToButton = () => {
+    // for selectors ...
+    const messages = $("#messages");
+    const newMessage = messages.children("li:last-child");
+    //for heights ...
+    const clientHeight = messages.prop("clientHeight");
+    const scrollTop = messages.prop("scrollTop");
+    const scrollHeight = messages.prop("scrollHeight");
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on("connect", () => {
     console.log("Connected to server");
 });
+
 socket.on("disconnect", () => {
     console.log("Server disconnected");
 });
@@ -15,6 +33,7 @@ socket.on("newMessage", (message) => {
         createdAt: timeFormat
     });
     $("#messages").append(html);
+    scrollToButton();
 });
 
 socket.on("newLocationMessage", (message) => {
@@ -26,6 +45,7 @@ socket.on("newLocationMessage", (message) => {
         createdAt: timeFormat
     });
     $("#messages").append(html);
+    scrollToButton();
 });
 
 $("#message-form").on("submit", (e) => {
